760d2ef0a6a1dbae30889af134479da3c5c8f853
ksdahmani10 <ks.dahmani10@gmail.com>
2023-08-01 10:43:19 -0700

    mand

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Define a class called Square"""
+class Square:
+    """Represents an empty square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class  Square"""
+
+
+class Square:
+    """Represents a square.
+    Private instance attribute.
+    Instantiation with size.
+    """
+
+    def __init__(self, size):
+        """Initializes the    data."""
+        self.__size = size
